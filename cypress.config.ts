3299318f4e23bad57ae15c6ba82483d9a1ba4d6e
aya-thafer2 <ayathafer2@gmail.com>
2023-11-11 14:45:12 +0200

    Install Cucumber

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsBuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    specPattern: "cypress/e2e/**/*.feature",
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsBuildPlugin(config)],
+        })
+      );
+      return config;
+    },
+  },
+});
