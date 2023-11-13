cb515770af1e246a1eb382709ef11596320b2a08
aya-thafer2 <ayathafer2@gmail.com>
2023-11-13 13:45:31 +0200

    General setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,33 +1,11 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsBuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 export default defineConfig({
   e2e: {
     baseUrl: 'https://opensource-demo.orangehrmlive.com',
     specPattern: "cypress/e2e/**/*.feature",
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
-      await addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsBuildPlugin(config)],
-        })
-      );
-      return config;
-    }, env: {
-      // allureReuseAfterSpec: true,
-      // download_dir: "./cypress/downloads",
-      snapshotOnly: true,
-      // allure: true,
+    async setupNodeEvents(on: any,config: any){
+      return require("./cypress/plugins")(on, config);
     },
-    // allure: true,
-    // allureResultsPath: "allure-results",
-    // videosFolder: "allure-results/",
-    // screenshotOnRunFailure: true,
   },
 });
